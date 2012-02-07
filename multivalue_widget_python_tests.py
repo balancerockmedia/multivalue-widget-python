9645a7ab87b7eb55b20067a1e4ffd65006d8f173
Daniel Johnson <dan@balancerockmedia.com>
2012-02-06 19:47:11 -0500

    added more tests

--- multivalue_widget_python_tests.py
+++ multivalue_widget_python_tests.py
@@ -30,11 +30,39 @@ class AppTestCase(unittest.TestCase):
         self.assertEqual(user.id, 1)
       
     def test_create_skill(self):
-        skill = Skill('Skiing', None)
+        skill = Skill('Sports', None)
         self.db.add(skill)
         self.db.commit()
         
         self.assertEqual(skill.id, 1)
+        
+    def test_create_user_with_skill(self):
+        user = User('Dan', 'Johnson')
+        self.db.add(user)
+        
+        skill = Skill('Sports', None)
+        self.db.add(skill)
+        
+        self.db.commit()
+        
+        user.skills.append(skill)
+        
+        self.assertEqual(len(user.skills), 1)
+        self.assertEqual(user.skills[0].name, 'Sports')
+        
+    def test_skill_tree(self):
+        sports = Skill('Sports', None)
+        self.db.add(sports)
+        
+        skiing = Skill('Skiing', sports)
+        self.db.add(skiing)
+        
+        soccer = Skill('Soccer', sports)
+        self.db.add(soccer)
+        
+        self.db.commit()
+        
+        self.assertEqual(skiing.parent.name, 'Sports')
         
 if __name__ == '__main__':
     unittest.main()
